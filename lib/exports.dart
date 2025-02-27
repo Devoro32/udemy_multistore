adf2f7468979ea6a76dafe8ba284ce5f91023c26
Devoro32 <edanebarton@gmail.com>
2025-02-27 19:23:58 +0000

    First commit

---
+++ lib/exports.dart
@@ -0,0 +1,13 @@
+//!core
+export 'package:flutter/material.dart';
+export 'package:firebase_auth/firebase_auth.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
+
+//!Controller
+
+//!Model
+
+//!View
+export 'package:multistore/views/screens/auth/login_screen.dart';
+export 'package:multistore/views/screens/auth/register_screen.dart';
